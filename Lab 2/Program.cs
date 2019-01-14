af82c7639b9e2a060f80670382b902f29aeb3180
stevojed <jedi1014@comcast.net>
2019-01-14 16:40:13 -0500

    Lab 2

---
+++ Lab 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+            while (repeat == true)
+            {
+                Console.WriteLine("Welcome to the wonderful world of measuring your class room");
+                Console.WriteLine("Please Dimentionalize Your Room!");
+
+                try
+                {
+                    Console.Write("Length: ");
+                    double L = double.Parse(Console.ReadLine());
+
+                    Console.Write("Width: ");
+                    double W = double.Parse(Console.ReadLine());
+
+                    Console.Write("Height: ");
+                    double H = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Area: " + (L * W));
+                    Console.WriteLine("Perimeter: " + ((2 * L) + (2 * W)));
+                    Console.WriteLine("Volume: " + (L * W * H));
+                }
+                catch
+                {
+                    Console.WriteLine("Try again");
+                }
+                bool again = true;
+                    while (again == true)
+                {
+                    Console.Write("Would you like to measure another room? Y/N: ");
+                    string go = Console.ReadLine();
+                    if ((go == "Y") || (go == "y") || (go == "Yes") || (go == "yes"))
+                    {
+                        again = false;
+                        repeat = true;
+                        Console.Clear();
+                    }
+                    else if ((go == "N") || (go == "n") || (go == "No") || (go == "no"))
+                    {
+                        again = false;
+                        repeat = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid response");
+                        again = true;
+                    }
+                }
+            }
+            Console.WriteLine("Press Any Key To Exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
